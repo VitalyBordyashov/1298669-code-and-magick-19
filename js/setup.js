5333696ef55295d29b333862b27ece0ce2c56b88
Vitaliy Bordyashov <vital.bordyashov>
2020-01-26 21:38:57 +0300

    Внес исправления, добавил функции в файл setap

--- js/setup.js
+++ js/setup.js
@@ -1,30 +1,43 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
+var creatingItem = function (value) {
+  return document.querySelector(value);
+}
+
+var userDialog = creatingItem('.setup');
 userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+creatingItem('.setup-similar').classList.remove('hidden');
 
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+var similarListElement = creatingItem('.setup-similar-list');
+var similarWizardTemplate = creatingItem('#similar-wizard-template')
     .content.querySelector('.setup-similar-item');
 
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристофор', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристофор', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
-var WIZARD_SURNAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
+var SURNAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 
-var coatColor = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var randomElement = function (Array) {
+  var randomN = Array[Math.floor(Math.random() * (Array.length - 1))];
+  return randomN;
+}
 for (var i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = WIZARD_NAMES[Math.floor(Math.random()*(WIZARD_NAMES.length-1))] + WIZARD_SURNAMES[i];
+  wizardElement.querySelector('.setup-similar-label').textContent = randomElement(NAMES) + randomElement(SURNAMES);
+
+  var propertyElement = wizardElement.querySelector('.wizard-coat');
+  propertyElement.style.fill = randomElement(coatColor);
+
+   var propertyElement2 = wizardElement.querySelector('.wizard-eyes');
+  propertyElement2.style.fill = randomElement(eyesColor);
 
   similarListElement.appendChild(wizardElement);
-
-  var propertyElement = document.querySelector('.wizard-coat').style.fill = coatColor[Math.floor(Math.random()*(coatColor.length-1))];
 }
+
 
 
