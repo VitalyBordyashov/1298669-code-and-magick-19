f61a4246e72e082a05b7bb54222e1f4601ab9e87
VitalyBordyashov <vital.bordyashov@mail.ru>
2020-01-27 13:31:33 +0300

    Устранил замечания наставника, объявил функции, упорядочил, переименовал

--- js/setup.js
+++ js/setup.js
@@ -1,43 +1,48 @@
 'use strict';
-
+// объявляем функцию создания эелемента DOM
 var creatingItem = function (value) {
   return document.querySelector(value);
-}
-
-var userDialog = creatingItem('.setup');
-userDialog.classList.remove('hidden');
-
-creatingItem('.setup-similar').classList.remove('hidden');
-
-var similarListElement = creatingItem('.setup-similar-list');
-var similarWizardTemplate = creatingItem('#similar-wizard-template')
-    .content.querySelector('.setup-similar-item');
-
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристофор', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-
-var SURNAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
-
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-
-var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-
+};
+// объявляем функцию случайного значения из массива
 var randomElement = function (Array) {
   var randomN = Array[Math.floor(Math.random() * (Array.length - 1))];
   return randomN;
-}
-for (var i = 0; i < 4; i++) {
+};
+// Объявляем функцию генерации персонажа:
+var characterGeneration = function () {
+  // создаем переменную (клон) персонажа
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
+  // создаем переменную, чтобы присвоить ей цвет мантии
+  var wizardCoatElement = wizardElement.querySelector('.wizard-coat');
+  // создаем переменную, чтобы присвоить ей цвет глаз
+  var wizardEyeesElement = wizardElement.querySelector('.wizard-eyes');
+  // присваиваем персонажу случайное имя и фамилию
   wizardElement.querySelector('.setup-similar-label').textContent = randomElement(NAMES) + randomElement(SURNAMES);
-
-  var propertyElement = wizardElement.querySelector('.wizard-coat');
-  propertyElement.style.fill = randomElement(coatColor);
-
-   var propertyElement2 = wizardElement.querySelector('.wizard-eyes');
-  propertyElement2.style.fill = randomElement(eyesColor);
-
+  // присваеваем стиль - случайный цвет мантии из приведенного списка.
+  wizardCoatElement.style.fill = randomElement(coatColor);
+  // присваеваем стиль - случайный цвет глаз из приведенного списка.
+  wizardEyeesElement.style.fill = randomElement(eyesColor);
+  // присваиваем все значения уже в DOM элемент вложенными "детьми"
   similarListElement.appendChild(wizardElement);
-}
-
-
-
+};
+// делаем видимым блок выбора похожего персонажа:
+creatingItem('.setup').classList.remove('hidden');
+// делаем видимым блок, в котором расположены похожие персонажи:
+creatingItem('.setup-similar').classList.remove('hidden');
+//Создаем переменную в которую будем добавлять созданные значения клонов персонажей:
+var similarListElement = creatingItem('.setup-similar-list');
+// Создаем переменную эелемента DOM значения которого будем клонировать:
+var similarWizardTemplate = creatingItem('#similar-wizard-template')
+    .content.querySelector('.setup-similar-item');
+// список имен для случайного выбора персонажу
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристофор', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+// список фамилий для случайного выбора персонажу
+var SURNAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
+// список цветов мантии для случайного выбора персонажу
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+// список цветов глаз для случайного выбра
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+// создаем цикл, чтобы получилось 4 клона и им присвоились имена, цвет мантии и глаз:
+for (var i = 0; i < 4; i++) {
+  characterGeneration();
+};
